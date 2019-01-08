0a8b2aff78a73bd356fee3872632a0764243a700
“wutingting” <1269878284@qq.com>
2019-01-08 16:58:54 +0800

    first jacocotestdemo

---
+++ jacoco/jacoco/src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -0,0 +1,25 @@
+package com.test.jacoco.test;
+
+/**
+ * Created by zc on 2017/5/2.
+ */
+
+import junit.framework.Assert;
+import org.junit.Test;
+import com.test.jacoco.HelloWorld;
+
+public class HelloWorldTest {
+    @Test
+    public void testMethod1() {
+        HelloWorld hw = new HelloWorld();
+        String ss = hw.testMethod1();
+        Assert.assertNotNull(ss);
+    }
+
+    @Test
+    public void testMethod2() {
+        HelloWorld hw = new HelloWorld();
+        int ss = hw.addMethod(1, 1);
+        Assert.assertEquals(ss, 2);
+    }
+}
